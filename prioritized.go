2b2b8101fbf62315760866d99551506e7854e15b
RenChen <renchenpro@gmail.com>
2020-11-23 16:42:54 +0800

    Update

--- prioritized.go
+++ prioritized.go
@@ -1,117 +1,147 @@
 package prioritized
 
-type PrioritizedWorkers struct {
-	highPriorityJobQueue chan *Job
-	midPriorityJobQueue  chan *Job
-	lowPriorityJobQueue  chan *Job
+//WorkerGroup ...
+type WorkerGroup struct {
+	highPriorityJobQueue chan *job
+	lowPriorityJobQueue  chan *job
 	jobTypeRegistry      map[interface{}]func(jobData interface{})
 	CloseSignal          chan struct{}
 }
 
-type Job struct {
+type job struct {
 	JobType interface{}
 	JobData interface{}
 }
 
-func Init(jobQueueCapacity int, jobWorkerConcurrency int, closeWorkerSignal chan struct{}) *PrioritizedWorkers {
-	s := &PrioritizedWorkers{
-		highPriorityJobQueue: make(chan *Job, jobQueueCapacity),
-		midPriorityJobQueue:  make(chan *Job, jobQueueCapacity),
-		lowPriorityJobQueue:  make(chan *Job, jobQueueCapacity),
+//Init ...
+func Init(jobQueueCapacity int, workerConcurrency int, closeWorkerSignal chan struct{}) *WorkerGroup {
+	s := &WorkerGroup{
+		highPriorityJobQueue: make(chan *job, jobQueueCapacity),
+		lowPriorityJobQueue:  make(chan *job, jobQueueCapacity),
+		jobTypeRegistry: make(map[interface{}]func(jobData interface{})),
 		CloseSignal:          closeWorkerSignal,
 	}
 	if closeWorkerSignal == nil {
 		s.CloseSignal = make(chan struct{})
 	}
-	for w := 0; w < jobWorkerConcurrency; w++ {
+	for w := 0; w < workerConcurrency; w++ {
 		go s.Worker(closeWorkerSignal)
 	}
 	return s
 }
 
-func (s *PrioritizedWorkers) Worker(closeSignal chan struct{}) {
+//Worker ...
+func (s *WorkerGroup) Worker(closeSignal chan struct{}) {
 	for {
 		select {
 		case <-closeSignal:
 			return
 		case j := <-s.highPriorityJobQueue:
-			s.HandleJob(j)
+			s.handleJob(j)
 		default:
 			{
 				select {
 				case <-closeSignal:
 					return
-				case j := <-s.midPriorityJobQueue:
-					s.HandleJob(j)
-				default:
-					{
-						select {
-						case <-closeSignal:
-							return
-						case j := <-s.lowPriorityJobQueue:
-							s.HandleJob(j)
-						default:
-							{
-								select {
-								case <-closeSignal:
-									return
-								case j := <-s.highPriorityJobQueue:
-									s.HandleJob(j)
-								case j := <-s.midPriorityJobQueue:
-									select {
-									case <-closeSignal:
-										return
-									case j := <-s.highPriorityJobQueue:
-										s.HandleJob(j)
-									default:
-									}
-									s.HandleJob(j)
-								case j := <-s.lowPriorityJobQueue:
-									select {
-									case <-closeSignal:
-										return
-									case j := <-s.highPriorityJobQueue:
-										s.HandleJob(j)
-									case j := <-s.midPriorityJobQueue:
-										s.HandleJob(j)
-									default:
-									}
-									s.HandleJob(j)
-								}
-							}
-						}
-					}
+				case j := <-s.highPriorityJobQueue:
+					s.handleJob(j)
+				case j := <-s.lowPriorityJobQueue:
+					s.handleJob(j)
 				}
 			}
 		}
 	}
 }
 
-func (s *PrioritizedWorkers) DefineJobType(typeSymbol interface{}, jobHandler func(jobData interface{})) {
+//DefineJobType ...
+func (s *WorkerGroup) DefineJobType(typeSymbol interface{}, jobHandler func(jobData interface{})) {
 	s.jobTypeRegistry[typeSymbol] = jobHandler
 }
 
-func (s *PrioritizedWorkers) HandleJob(j *Job) {
+
+//handleJob ...
+func (s *WorkerGroup) handleJob(j *job) {
 	s.jobTypeRegistry[j.JobType](j.JobData)
 }
 
-func (s *PrioritizedWorkers) PendHighPriorityJob(typeSymbol interface{}, jobData interface{}) {
-	s.highPriorityJobQueue <- &Job{
+
+//PendHighPriorityJob ...
+func (s *WorkerGroup) PendHighPriorityJob(typeSymbol interface{}, jobData interface{}) {
+	s.highPriorityJobQueue <- &job{
 		JobType: typeSymbol,
 		JobData: jobData,
 	}
 }
 
-func (s *PrioritizedWorkers) PendMidPriorityJob(typeSymbol interface{}, jobData interface{}) {
-	s.midPriorityJobQueue <- &Job{
+//PendLowPriorityJob ...
+func (s *WorkerGroup) PendLowPriorityJob(typeSymbol interface{}, jobData interface{}) {
+	s.lowPriorityJobQueue <- &job{
 		JobType: typeSymbol,
 		JobData: jobData,
 	}
 }
 
-func (s *PrioritizedWorkers) PendLowPriorityJob(typeSymbol interface{}, jobData interface{}) {
-	s.lowPriorityJobQueue <- &Job{
-		JobType: typeSymbol,
-		JobData: jobData,
+//PendAutoFallback ...
+func (s *WorkerGroup) PendAutoFallback(typeSymbol interface{}, jobData interface{}) {
+	select {
+		case s.highPriorityJobQueue <- &job{
+			JobType: typeSymbol,
+			JobData: jobData,
+		}: {}
+		case s.lowPriorityJobQueue <- &job{
+			JobType: typeSymbol,
+			JobData: jobData,
+		}:{}
+	}
+}
+
+//TryPendHighPriorityJob ...
+func (s *WorkerGroup) TryPendHighPriorityJob(typeSymbol interface{}, jobData interface{}) bool {
+	select {
+		case s.highPriorityJobQueue <- &job{
+			JobType: typeSymbol,
+			JobData: jobData,
+		}: {
+			return true
+		}
+		default: {
+			return false
+		}
+	}
+}
+
+//TryPendLowPriorityJob ...
+func (s *WorkerGroup) TryPendLowPriorityJob(typeSymbol interface{}, jobData interface{}) bool {
+	select {
+		case s.lowPriorityJobQueue <- &job{
+			JobType: typeSymbol,
+			JobData: jobData,
+		}: {
+			return true
+		}
+		default: {
+			return false
+		}
+	}
+}
+
+//TryPendAutoFallback ...
+func (s *WorkerGroup) TryPendAutoFallback(typeSymbol interface{}, jobData interface{}) bool {
+	select {
+		case s.highPriorityJobQueue <- &job{
+			JobType: typeSymbol,
+			JobData: jobData,
+		}: {
+			return true
+		}
+		case s.lowPriorityJobQueue <- &job{
+			JobType: typeSymbol,
+			JobData: jobData,
+		}: {
+			return true
+		}
+		default: {
+			return false
+		}
 	}
 }
